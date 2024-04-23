a723d9c5f240d6e6b50b2c2d127fb096374e0f5c
DillonCislo <dilloncislo@gmail.com>
2024-04-22 21:02:56 -0400

    A bunch of PEP8 compliance nonsense

--- pcmls/pcmls.py
+++ pcmls/pcmls.py
@@ -51,16 +51,17 @@ def _pcmls_wendland(v, h):
     """
     
     d2 = jnp.sum(v**2, axis=1)
-    in_bnds = jnp.logical_and(0 < d2, d2 < h**2)
-    d2 = jnp.where(in_bnds, d2, 1)
+    in_bounds = jnp.logical_and(0 < d2, d2 < h**2)
+    d2 = jnp.where(in_bounds, d2, 1)
     d = jnp.sqrt(d2)
 
-    w = jnp.where(in_bnds, (1-(d/h))**4 * (4*(d/h)+1), 0)    
+    w = jnp.where(in_bounds, (1-(d/h))**4 * (4*(d/h)+1), 0)
     return w
+
 
 # List of weight functions
 _all_pcmls_weight_functions = [_pcmls_gaussian, _pcmls_wendland]
-_all_pcmls_weight_index_maps = { 'gaussian': 0, 'wendland': 1 }
+_all_pcmls_weight_index_maps = {'gaussian': 0, 'wendland': 1}
 
 
 # Custom rule for computing monomials with integer powers
@@ -79,16 +80,16 @@ def _pcmls_int_pow_jvp(primals, tangents):
 
 
 @jit
-def _eval_pcmls(q, p, FP_array, power_array, weight_function_index=0, weight_param=1):
+def _eval_pcmls(q, p, fp_array, power_array, weight_function_index=0, weight_param=1):
     """
     Workhorse function for evaluating the MLS interpolant and its derivatives
 
     Args:
     q (array-like): dim-element 1D array.
         The current query point.
-    P (array-like): (num_sources) x dim matrix.
+    p (array-like): (num_sources) x dim matrix.
         The complete set of source points.
-    FP_array (array_like): (num_monomials) x (num_sources) matrix.
+    fp_array (array_like): (num_monomials) x (num_sources) matrix.
         Scalar function values at source points tiled for each monomial.
     power_array (array_like): (num_monomials) x dim x (num_sources).
         Integer powers defining each monomial term in the interpolant.
@@ -101,31 +102,30 @@ def _eval_pcmls(q, p, FP_array, power_array, weight_function_index=0, weight_par
         float: The interpolated value of the scalar function at 'q'.
     """
 
-    q = q[None,:]
-    # dim = q.shape[1]
-    # num_sources = p.shape[0]
-    num_monomials = FP_array.shape[0]
+    q = q[None, :]
+    num_monomials = fp_array.shape[0]
     
-    v = p - q # P x dim
+    v = p - q  # P x dim
     w = lax.switch(weight_function_index, _all_pcmls_weight_functions,
                    v, weight_param)
     
     # Compute monomial basis functions
-    v = jnp.tile(jnp.transpose(v[:, :, None], (2,1,0)),
-                 (num_monomials, 1, 1)) # k x dim x P
+    v = jnp.tile(jnp.transpose(v[:, :, None], (2, 1, 0)),
+                 (num_monomials, 1, 1))  # k x dim x P
     b = _pcmls_int_pow(v, power_array)
-    b = jnp.prod(b, axis=1) # k x P
+    b = jnp.prod(b, axis=1)  # k x P
 
     w = jnp.tile(w[None, :], (num_monomials, 1))
     wb = w * b  # k x P
 
     # Construct and solve the linear problem
-    rhs = jnp.sum(wb * FP_array, axis=1) # k x 1
-    lhs = jnp.sum(wb[None, :, :] * b[:, None, :], axis=2).T # k x k
+    rhs = jnp.sum(wb * fp_array, axis=1)  # k x 1
+    lhs = jnp.sum(wb[None, :, :] * b[:, None, :], axis=2).T  # k x k
     lhs = (lhs + lhs.T) / 2
     c = jsp.linalg.solve(lhs, rhs[:, None], assume_a='sym')
 
     return jnp.squeeze(c[0])
+
 
 # Scalar derivatives of the workhorse function
 _grad_pcmls = jit(grad(_eval_pcmls, argnums=0))
@@ -135,6 +135,7 @@ _hess_pcmls = jit(hessian(_eval_pcmls, argnums=0))
 _veval_pcmls = jit(vmap(_eval_pcmls, in_axes=(0, None, None, None, None, 0)))
 _vgrad_pcmls = jit(vmap(_grad_pcmls, in_axes=(0, None, None, None, None, 0)))
 _vhess_pcmls = jit(vmap(_hess_pcmls, in_axes=(0, None, None, None, None, 0)))
+
 
 def enumerate_monomials(m, dim):
     """
@@ -150,7 +151,7 @@ def enumerate_monomials(m, dim):
     """
 
     divider_positions = list(combinations(range(m+dim), dim))
-    num_monomials= len(divider_positions)
+    num_monomials = len(divider_positions)
     power_matrix = np.ones((num_monomials, m+dim), dtype=int)
 
     for row, cols in enumerate(divider_positions):
@@ -169,7 +170,7 @@ def enumerate_monomials(m, dim):
     return power_matrix
 
 
-def pcmls(Q, P, Fp, m=2, weight_function='gaussian', weight_method='knn',
+def pcmls(q, p, fp, m=2, weight_function='gaussian', weight_method='knn',
           weight_param=10, vectorized=False, compute_gradients=True,
           compute_hessians=True, verbose=False):
     """
@@ -177,11 +178,11 @@ def pcmls(Q, P, Fp, m=2, weight_function='gaussian', weight_method='knn',
     function defined at scattered data points
 
     Args:
-    Q (array-like): (num_queries) x dim matrix.
+    q (array-like): (num_queries) x dim matrix.
         List of query points at which to evalute the interpolant/derivatives.
-    P (array-like): (num_sources) x dim matrix.
+    p (array-like): (num_sources) x dim matrix.
         List of source points at which the scalar function is defiend.
-    Fp (array_like): (num_sources)-element 1D or 2D vector
+    fp (array_like): (num_sources)-element 1D or 2D vector
         Values of the scalar function at the source points.
     m (int, optional): Maximal degree of the polynomial interpolant.
         Defaults to 2.
@@ -214,21 +215,23 @@ def pcmls(Q, P, Fp, m=2, weight_function='gaussian', weight_method='knn',
         The interpolated function Hessian matrices at the query points.
     """
 
-    if Q.ndim == 1: Q = Q[None, :]
-    assert Q.ndim == 2, 'Please supply query points as a #Q x dim matrix'
-    num_queries = Q.shape[0]
-    dim = Q.shape[1]
+    if q.ndim == 1:
+        q = q[None, :]
+    assert q.ndim == 2, 'Please supply query points as a #q x dim matrix'
+    num_queries = q.shape[0]
+    dim = q.shape[1]
 
-    assert (P.ndim == 2 and P.shape[1] == dim), \
-        'Please supply source points as a #P x dim matrix'
-    num_sources = P.shape[0]
+    assert (p.ndim == 2 and p.shape[1] == dim), \
+        'Please supply source points as a #p x dim matrix'
+    num_sources = p.shape[0]
 
-    assert Fp.size == num_sources, 'Scalar function input must have #P elements'
-    if Fp.ndim == 1: Fp = Fp[None, :]
-    if Fp.ndim == 2:
-        if Fp.shape[0] != 1:
-            Fp = Fp.T
-            if Fp.shape[0] != 1:
+    assert fp.size == num_sources, 'Scalar function input must have #p elements'
+    if fp.ndim == 1:
+        fp = fp[None, :]
+    if fp.ndim == 2:
+        if fp.shape[0] != 1:
+            fp = fp.T
+            if fp.shape[0] != 1:
                 raise ValueError('Scalar function must be supplied as a vector')
     else:
         raise ValueError('Scalar function must be supplied as a vector')
@@ -243,12 +246,12 @@ def pcmls(Q, P, Fp, m=2, weight_function='gaussian', weight_method='knn',
 
     # Handle weight parameters
     if weight_method == 'scalar':      
-       weight_param = weight_param * np.ones((num_queries,1))
+        weight_param = weight_param * np.ones((num_queries,1))
         
     elif weight_method == 'knn':      
         nn = NearestNeighbors(n_neighbors=weight_param)
-        nn.fit(P)
-        weight_param, _ = nn.kneighbors(Q)
+        nn.fit(p)
+        weight_param, _ = nn.kneighbors(q)
         weight_param = weight_param[:, -1]
         
     elif weight_method == 'knnmontecarlo':      
@@ -263,64 +266,72 @@ def pcmls(Q, P, Fp, m=2, weight_function='gaussian', weight_method='knn',
     power_array = np.tile(power_array[:, :, None], (1, 1, num_sources))
 
     # Re-size the scalar function array (see eval_pcmls)
-    Fp_array = np.tile(Fp, (num_monomials, 1))
+    fp_array = np.tile(fp, (num_monomials, 1))
 
     # Convert everything to JAX style arrays
-    Q = jnp.array(Q)
-    P = jnp.array(P)
-    Fp_array = jnp.array(Fp_array)
+    q = jnp.array(q)
+    p = jnp.array(p)
+    fp_array = jnp.array(fp_array)
     weight_param = jnp.array(weight_param)
     weight_param = weight_param[:, None]
     power_array = jnp.array(power_array)
 
     if vectorized:
 
-        if verbose: print('Computing function values... ', end='')
-        Fq = _veval_pcmls(Q, P, Fp_array, power_array, 
+        if verbose:
+            print('Computing function values... ', end='')
+        fq = _veval_pcmls(q, p, fp_array, power_array,
                          weight_function_index, weight_param)
-        if verbose: print('Done')
+        if verbose:
+            print('Done')
         
         if compute_gradients:
-            if verbose: print('Computing gradients... ', end='')
-            DFq = _vgrad_pcmls(Q, P, Fp_array, power_array,
-                              weight_function_index, weight_param)
-            if verbose: print('Done')
+            if verbose:
+                print('Computing gradients... ', end='')
+            grad_fq = _vgrad_pcmls(q, p, fp_array, power_array,
+                                   weight_function_index, weight_param)
+            if verbose:
+                print('Done')
         else:
-            DFq = None
+            grad_fq = None
             
         if compute_hessians:
-            if verbose: print('Computing Hessians... ', end='')
-            HFq = _vhess_pcmls(Q, P, Fp_array, power_array,
-                              weight_function_index, weight_param)
-            if verbose: print('Done')
+            if verbose:
+                print('Computing Hessians... ', end='')
+            hess_fq = _vhess_pcmls(q, p, fp_array, power_array,
+                                   weight_function_index, weight_param)
+            if verbose:
+                print('Done')
         else:
-            HFq = None
+            hess_fq = None
 
     else:
 
-        Fq = np.zeros((num_queries, 1))
-        DFq = np.zeros((num_queries, dim)) if compute_gradients else None
-        HFq = np.zeros((num_queries, dim, dim)) if compute_hessians else None
+        fq = np.zeros((num_queries, 1))
+        grad_fq = np.zeros((num_queries, dim)) \
+            if compute_gradients else None
+        hess_fq = np.zeros((num_queries, dim, dim)) \
+            if compute_hessians else None
 
         iterable = (progressbar(range(num_queries))
                     if verbose else
                     range(num_queries))
         for i in iterable:
-            Fq[i] = _eval_pcmls(Q[i,:], P, Fp_array, power_array,
-                               weight_function_index, weight_param[i])
+            fq[i] = _eval_pcmls(q[i, :], p, fp_array, power_array,
+                                weight_function_index, weight_param[i])
             
             if compute_gradients:
-                DFq[i, :] = _grad_pcmls(Q[i,:], P, Fp_array, power_array,
-                                       weight_function_index, weight_param[i])
+                grad_fq[i, :] = _grad_pcmls(q[i, :], p, fp_array, power_array,
+                                            weight_function_index, weight_param[i])
 
             if compute_hessians:
-                HFq[i, :, :] = _hess_pcmls(Q[i,:], P, Fp_array, power_array,
-                                          weight_function_index, weight_param[i])
+                hess_fq[i, :, :] = _hess_pcmls(q[i, :], p, fp_array, power_array,
+                                               weight_function_index, weight_param[i])
 
     # Restore output to Numpy style arrays
-    Fq = np.array(Fq.ravel())
-    DFq = np.array(DFq)
-    HFq = np.array(HFq)
+    fq = np.array(fq.ravel())
+    grad_fq = np.array(grad_fq)
+    hess_fq = np.array(hess_fq)
     
-    return Fq, DFq, HFq
+    return fq, grad_fq, hess_fq
 

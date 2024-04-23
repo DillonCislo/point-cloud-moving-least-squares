ea45b76d8dde7a10678b82ed71fbcac6ee7552e0
DillonCislo <dilloncislo@gmail.com>
2024-04-23 10:25:14 -0400

    Some typos in the setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 import setuptools
 
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='point_cloud_moving_least_squares',
     version='0.1.0',
     packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
-        'scipy'
+        'scipy',
         'jax',
         'jaxlib',
-        'scikit-learn'
+        'scikit-learn',
         'progressbar2'
     ],
     author='Dillon Cislo',
     author_email='dilloncislo@gmail.com',
-    description='An implementation of Moving Least Squares for N-dimensional point clouds.'
+    description='An implementation of Moving Least Squares for N-dimensional point clouds.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
